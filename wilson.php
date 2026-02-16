dfd86373b36c1e8d35992d78b1eb401108faca3b
James Adrian Bibon <jamesbarrameda01@gmail.com>
2026-02-16 09:08:16 +0800

    Add files via upload

---
+++ wilson.php
@@ -0,0 +1,15 @@
+<?php
+$data = file_get_contents("dataset.json");
+$data = json_decode($data);
+
+foreach ($data as $person) {
+    if ($person->address->city == "Quezon City") {
+        
+        echo $person->id . " - " .
+             $person->first_name . " " .
+             $person->last_name .
+             " (" . $person->address->city . ")";
+        echo "<\n>";
+    }
+}
+?>
