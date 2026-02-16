79b99e51453f6a4524803c16ff68dc40e4ac6974
jeymsdrei <jamesbarrameda01@gmail.com>
2026-02-16 10:33:02 +0800

    parot

--- wilson.php
+++ wilson.php
@@ -3,7 +3,7 @@ $data = file_get_contents("dataset.json");
 $data = json_decode($data);
 
 foreach ($data as $person) {
-    if ($person->address->city == "Quezon City") {
+    if ($person->address->city == "n City") {
         
         echo $person->id . " - " .
              $person->first_name . " " .
